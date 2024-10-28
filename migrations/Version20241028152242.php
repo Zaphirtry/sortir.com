3e366f5887743cc320ce47f3340d9a4befbaae18
Zaphirtry <defcorentin@outlook.fr>
2024-10-28 16:30:26 +0100

    correction doublon home et migrations

--- migrations/Version20241028152242.php
+++ migrations/Version20241028152242.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241028142008 extends AbstractMigration
+final class Version20241028152242 extends AbstractMigration
 {
     public function getDescription(): string
     {
