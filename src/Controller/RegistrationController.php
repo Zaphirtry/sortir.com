329c0901add27cf7b1ec195518edd58787679389
Zaphirtry <defcorentin@outlook.fr>
2024-10-30 15:49:10 +0100

    page de creation de'un nouvel utilisateur seulement par l'admin

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\CsvImportType;
 use App\Form\RegistrationFormType;
+use App\Service\CsvImportService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -15,27 +17,49 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register' ,methods: ['POST','GET'])]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    #[Route('/register', name: 'app_register', methods: ['POST', 'GET'])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, CsvImportService $importService, Security $security, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
-        if (!$this->getUser()){
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('main_home');
+        }
+
+        // Formulaire d'importation CSV
+        $csvForm = $this->createForm(CsvImportType::class);
+        $csvForm->handleRequest($request);
+
+        if ($csvForm->isSubmitted() && $csvForm->isValid()) {
+            $csvFile = $csvForm->get('csvFile')->getData();
+
+            if ($csvFile) {
+                $filePath = $csvFile->getPathname();
+                $results = $importService->importUsers($filePath);
+
+                if ($results['success'] > 0) {
+                    $this->addFlash('success', "{$results['success']} utilisateurs ont été importés avec succès.");
+                }
+
+                foreach ($results['errors'] as $error) {
+                    $this->addFlash('error', $error);
+                }
+            }
+            return $this->redirectToRoute('admin_index');
+        }
+
+        // Formulaire d'enregistrement d'utilisateur
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+        $registrationForm = $this->createForm(RegistrationFormType::class, $user);
+        $registrationForm->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
-
-            // encode the plain password
+        if ($registrationForm->isSubmitted() && $registrationForm->isValid()) {
+            $plainPassword = $registrationForm->get('plainPassword')->getData();
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $date = new \DateTimeImmutable('now');
             $user->setDateCreated($date);
             $user->setIsActive(true);
 
-            //ajoute l'image si elle est présente
-            $uploadedImage = $form->get('file')->getData();
+            $uploadedImage = $registrationForm->get('file')->getData();
             if ($uploadedImage) {
                 $originalImageName = pathinfo($uploadedImage->getClientOriginalName(), PATHINFO_FILENAME) . '.' . $uploadedImage->guessExtension();
                 $safeImageName = $slugger->slug($originalImageName);
@@ -45,25 +69,20 @@ class RegistrationController extends AbstractController
                     $uploadedImage->move($this->getParameter('uploads_images_directory'), $newImageName);
                     $user->setFilename($newImageName);
                 } catch (\Exception $e) {
-                    // Ajoutez un message flash d'erreur
                     $this->addFlash('error', "Une erreur s'est produite lors du téléchargement de l'image. Veuillez réessayer.");
                 }
             }
 
             $entityManager->persist($user);
             $entityManager->flush();
-            //ajout d'un message flash
-            $this->addFlash('success',"Compte créé ! Bienvenue !");
-            // do anything else you need here, like send an email
+            $this->addFlash('success', "Utilisateur ajouté !");
 
-            return $security->login($user, 'form_login', 'main');
+            return $this->redirectToRoute('admin_index');
         }
 
         return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
+            'registrationForm' => $registrationForm->createView(),
+            'csvForm' => $csvForm->createView(),
         ]);
-        }else{
-            return $this->redirectToRoute('main_home');
-        }
     }
 }
